e7929a1f3af386cf9d75991616cdf81115597f8e
Melissa Yates <melissampeart@gmail.com>
2019-08-22 16:45:44 -0500

    class work

---
+++ basics2fcw.py
@@ -0,0 +1,68 @@
+# # Python Basics: Math and String Formatting
+#
+# ### Problem 1:
+# Write some Python code that has three variables called ```greeting```, ```my_name```, and ```my_age```.
+# Intialize each of the 3 variables with an appropriate value, then print out the example below using the 3 variables and
+# two different approaches for formatting Strings.
+#
+# 1) Using concatenation and the ```+``` and 2) Using an ```f-string```. Sample output:
+#
+# ```
+# YOUR_GREETING_VARIABLE YOUR_NAME_VARIABLE!!! I hear that you are YOUR_MY_AGE_VARIABLE today!
+# ```
+#
+greeting = "Hello"
+my_name = "Melissa Yates"
+my_age = 33
+
+# print(greeting + ", My name " + " is " + my_name + ". I am " + str(my_age) + " years old.")
+# print(f"{greeting}, My name is {my_name}. I am {str(my_age)} years old.")
+# ### Problem 2:
+# Write some Python code that asks the user for a secret password.
+# Create a loop that quits with the user's quit word. If the user doesn't enter that word, ask them to guess again.
+#
+# secret_password = input("Enter a password ")
+# userInput = " "
+# while userInput != secret_password:
+#     userInput = input("Enter the password confirmation. Enter q to quit ")
+#     if userInput == secret_password:
+#         print("You are correct")
+#     else:
+#         print("Guess again")
+
+
+
+
+
+# ### Problem 3:
+# Write some Python code using ```f-strings``` that prints 0 to 50 three times in a row (vertically).
+# ```
+# 1 1 1
+# 2 2 2
+# 3 3 3
+# 4 4 4
+# 5 5 5
+# .
+# .
+# ```
+#
+for i in range(1, 50+1):
+    print(f"{i} {i} {i}")
+
+# ### Problem 4:
+# Write some Python code that create a random number and stores it in a variable. Ask the user to guess the random number.
+# Keep letting the user guess until they get it right, then quit.
+#
+# random_number = random.randint(1,10)
+# userGuess = " "
+# while userGuess != 'q':
+#     userGuess = int(input("Guess a random number. Enter 'q' to quit "))
+#     if userGuess != random_number:
+#         print("Sorry, Incorrect ")
+#     else:
+#         print("Correct")
+# ### Challenge
+# Write some Python code to ask the user to create a number for the computer to guess between 1 - 10000.
+# Write the code so that the computer guesses random numbers between 1 - 10000 and will keep guessing until the computer guesses
+# the number correctly. Once the computer guesses the random number, alert the user with an alert box that displays how many guesses
+# it took to guess the random number.
